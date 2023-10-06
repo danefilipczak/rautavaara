dd33dc9c7ba94eb62bcd0f49e358a4eae17409c9
Dane Filipczak <dane@imaginationrock.com>
2023-10-06 18:08:19 -0400

    resizeable viewer

--- src/rautavaara/viewer.cljs
+++ src/rautavaara/viewer.cljs
@@ -1,10 +1,13 @@
 (ns rautavaara.viewer
   (:require [leramir.ast :as ast]
             [leramir.types.timed-value :as tv]
-            [leramir.utils.math :as utils.math] 
+            [leramir.utils.math :as utils.math]
             [leramir.core :as core]
+            [react :as react]
+            [reagent.core]
             [rational.core :as r]))
 
+(defn spy [x] (.log js/console x) x)
 (defn pitch-set [x]
   ;;eventually pitch/pitch-set, but that isn't stable at this time because the protocol implememntation isn't clj-safe
   ;;
@@ -14,7 +17,7 @@
         #{x}
         :else #{}))
 
-(defn piano-roll [width height path-value-map]
+(defn piano-roll* [width height path-value-map]
   (let [pitches (sort (mapcat (comp pitch-set tv/value) (vals path-value-map)))
         min-pitch (first pitches)
         max-pitch (last pitches)
@@ -24,35 +27,36 @@
         rescale-tv-time (fn [tv-time] (float (utils.math/map-range (r/realize tv-time) 0 last-end 0 width)))
         nn->i (zipmap stave (range))
         nn->height (fn [nn]
-                     (.log js/console (print-str nn))
+                     #_(.log js/console (print-str nn))
                      (assert (int? nn))
                      (* (nn->i nn) height-per-stave))]
-    (concat (for [[i nn] (partition 2 (interleave (range) stave))]
-              [:rect
-               {:stroke :gainsboro
-                :stroke-width 0.5
-                :fill (if (contains?
-                           #{0 2 4 5 7 9 11}
-                           (int (mod nn 12)))
-                        :white
-                        :whitesmoke)
-                :x 0
-                :y (nn->height nn)
-                :width width
-                :height height-per-stave}])
-            (for [tv (vals path-value-map)
-                  :let [pitch-set (pitch-set (tv/value tv))
-                        color (:color (tv/attrs tv))]
-                  nn pitch-set
-                  :when nn]
-              [:rect
-               {:stroke :gainsboro
-                :stroke-width 0.5
-                :fill (or color :black)
-                :x (rescale-tv-time (tv/start tv))
-                :y (nn->height nn)
-                :width (rescale-tv-time (tv/duration tv))
-                :height height-per-stave}]))))
+    [:svg {:width width :height height}
+     (concat (for [[i nn] (partition 2 (interleave (range) stave))]
+               [:rect
+                {:stroke :gainsboro
+                 :stroke-width 0.5
+                 :fill (if (contains?
+                            #{0 2 4 5 7 9 11}
+                            (int (mod nn 12)))
+                         :white
+                         :whitesmoke)
+                 :x 0
+                 :y (nn->height nn)
+                 :width width
+                 :height height-per-stave}])
+             (for [tv (vals path-value-map)
+                   :let [pitch-set (pitch-set (tv/value tv))
+                         color (:color (tv/attrs tv))]
+                   nn pitch-set
+                   :when nn]
+               [:rect
+                {:stroke :gainsboro
+                 :stroke-width 0.5
+                 :fill (or color :black)
+                 :x (rescale-tv-time (tv/start tv))
+                 :y (nn->height nn)
+                 :width (rescale-tv-time (tv/duration tv))
+                 :height height-per-stave}]))]))
 
 (defn bracket [from to height]
   (let [attrs {:stroke :black}]
@@ -82,19 +86,16 @@
 (defn translate [x y & children]
   (into [:g {:transform (str "translate(" x " " y ")")}] children))
 
-(defn vizualize-era [era]
+(defn voice-tree [{:keys [era width]}]
   (let [path-value-map (core/era->path-value-map era)
-        width 800
         per-rung 10
         rung-padding 5
-        rung-total (+ rung-padding per-rung)
-        piano-roll-height 300
+        rung-total (+ rung-padding per-rung) 
         metas (into {} (filter (fn [[path tv]] (keyword? (tv/value tv))) path-value-map)) ;; todo -> rewrite with specter
         max-count (apply max (map count (keys metas)))
         last-end (last (sort (map (comp r/realize tv/end) (vals path-value-map))))
         rescale-tv-time (fn [tv-time] (float (utils.math/map-range (r/realize tv-time) 0 last-end 0 width)))
-        margin-height (* rung-total (inc max-count))
-        total-height (+ margin-height piano-roll-height)]
+        total-height (* rung-total (inc max-count))]
     [:svg
      {:width width
       :height total-height}
@@ -109,22 +110,85 @@
                   (bracket (rescale-tv-time (tv/start tv))
                            (rescale-tv-time (tv/end tv))
                            per-rung)))
-     (translate 0 margin-height
+     #_(translate 0 margin-height
                 (piano-roll width piano-roll-height path-value-map))]))
 
+(defn piano-roll [{:keys [era width height] :as attrs}] 
+  [piano-roll* width height (core/era->path-value-map era)])
+
+(defn resizable-div* [_component _attrs]
+  (let [size (reagent.core/atom {:width 0 :height 300})
+        resize-observer (js/ResizeObserver. (fn [entries _] 
+                                              (when-let [entry (first entries)]
+                                                (.log js/console (.-target entry))
+                                                (let [dom-rect (.-contentRect entry)]
+                                                  (spy entry)
+                                                  (spy dom-rect)
+                                                  (reset! size {:width (.-width dom-rect)
+                                                                :height (spy (- (.-height dom-rect) 0 #_6.5))})))))]
+    (fn [component attrs]
+      (let [my-ref (react/useRef)]
+        (react/useEffect
+         (fn []
+           (println my-ref)
+           (when (.-current my-ref)
+             (.observe resize-observer (.-current my-ref)))
+           (fn []
+             (.disconnect resize-observer)))
+         (clj->js []))
+        
+        [:div.vertical-resize
+         {:ref my-ref
+          :style {:height (:height @size)}}
+         [component (merge attrs @size)]]))))
+
+(defn resizable-div [component attrs]
+  [:f> resizable-div* component attrs])
+
+(defn visualize-era [{:keys [era width] :as attrs}]
+  [:<>
+   [resizable-div voice-tree attrs]
+   [resizable-div piano-roll attrs]])
+
+(defn editor [era on-change]
+  [visualize-era
+   {:era era
+    :on-change on-change}])
+
 (defn root []
-  [:<> 
-   [:h1 "there there, it's not all bad"]
-   (doall (vizualize-era [:era 2 [:era {:color "green"} 2 3] [2 3]]))
-   [:br] 
-   (doall (vizualize-era [:era 2 2]))
-   [:br]
-   "hi is what I say to my friends. They don't neccessarily love it, but they put up with it."
-   [:svg]])
+  (let [era (reagent.core/atom 
+             [:era 2 3 #{4 5 6} [:heap 2
+                                 [:era  2 3]
+                                 [2 3]
+                                 1 3 5 7 13]])]
+    (fn []
+      [:<>
+       [:div.notebook
+        [:h1 "UI ideas"]]
+       [editor @era (fn [new-era]
+                      (reset! era new-era))] 
+       [:br]
+       [:svg]])))
 
 
-(comment 
-  (vizualize-era [[:graft {:color "green"} 2] 4 [ 5]])
+(comment
+  ;; todo -- navigation
+  ;; cross siblings - 'horizontal'
+  ;; accross levels - 'vertical'
+
+  ;; todo -- zooming
+  ;; you can zoom up to the current level that you're at
+  ;; moving to a sibling shifts the zoom to the sibling
+  ;; moving to a parent shifts the zoom to the parent
+
+  ;; todo - editing
+  ;; slup
+  ;; barf
+  ;; moving pitch values up and down
+  ;; move up/down by octave
   
-  (ast/standard-interpretation [1 2 3])
-  )
+
+  (resizable-div
+   (vizualize-era [[:graft {:color "green"} 2] 4 [5]]))
+
+  (ast/standard-interpretation [1 2 3]))
