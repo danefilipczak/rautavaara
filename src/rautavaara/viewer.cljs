249ae23fdbfba1f31fd470f15c0cec64a0cb6a36
Dane Filipczak <dane@imaginationrock.com>
2023-10-07 09:45:55 -0400

    basic nav controls

--- src/rautavaara/viewer.cljs
+++ src/rautavaara/viewer.cljs
@@ -5,7 +5,12 @@
             [leramir.core :as core]
             [react :as react]
             [reagent.core]
+            [clojure.pprint]
             [rational.core :as r]))
+
+(defn pprint [x]
+  [:pre
+   (with-out-str (clojure.pprint/pprint x))])
 
 (defn spy [x] (.log js/console x) x)
 (defn pitch-set [x]
@@ -17,23 +22,76 @@
         #{x}
         :else #{}))
 
-(defn piano-roll* [width height path-value-map]
-  (let [pitches (sort (mapcat (comp pitch-set tv/value) (vals path-value-map)))
+;;; begin move into core.ast
+
+(defn era? [ast]
+  (= ::leramir.ast/era (:type ast)))
+
+(defn values* [result ast]
+  (if (era? ast)
+    (apply concat result (map (partial values* result) (:children ast)))
+    (conj result ast)))
+
+(defn values [ast]
+  (values* [] ast))
+
+(defn value [ast] (:value ast))
+
+(defn end [ast] (r/+ (:start ast) (:duration ast)))
+
+(defn start [ast] (:start ast))
+(defn duration [ast] (:duration ast))
+(defn attrs [ast] (:percolated-attrs ast))
+
+(defn sub-path? [path-1 path-2]
+  (= path-1 (vec (take (count path-1) path-2))))
+
+(defn up [ast path]
+  (when (> (count path) 0)
+    (:path (get-in ast (leramir.ast/ast-path (pop path))))))
+
+(defn down [ast path]
+  (:path (get-in ast (leramir.ast/ast-path (conj path 0)))))
+
+(defn left [ast path]
+  (when (and (not-empty path) (> (peek path) 0))
+    (:path (get-in ast (leramir.ast/ast-path (conj (pop path) (dec (peek path))))))))
+
+(defn right [ast path]
+  (when (not-empty path)
+    (:path (get-in ast (leramir.ast/ast-path (conj (pop path) (inc (peek path))))))))
+
+(defn nearest-era [ast path]
+  (if (era? (get-in ast (leramir.ast/ast-path path)))
+    ast
+    (:path (get-in ast (leramir.ast/ast-path (pop path))))))
+
+(comment
+
+  (parent [13 2] (leramir.ast/standard-interpretation [1 2 3]))
+  (pop [1 2 3 4]))
+
+;;; end move into core.ast
+
+(def highlight-color "#AADE91")
+
+(defn piano-roll* [{:keys [width height ast active-path]}]
+  (let [pitches (sort (mapcat (comp pitch-set value) (values ast)))
         min-pitch (first pitches)
         max-pitch (last pitches)
         stave (reverse (range min-pitch (inc max-pitch)))
         height-per-stave (float (/ height (count stave)))
-        last-end (last (sort (map (comp r/realize tv/end) (vals path-value-map))))
+        last-end (last (sort (map (comp r/realize end) (values ast))))
         rescale-tv-time (fn [tv-time] (float (utils.math/map-range (r/realize tv-time) 0 last-end 0 width)))
         nn->i (zipmap stave (range))
         nn->height (fn [nn]
-                     #_(.log js/console (print-str nn))
                      (assert (int? nn))
                      (* (nn->i nn) height-per-stave))]
     [:svg {:width width :height height}
      (concat (for [[i nn] (partition 2 (interleave (range) stave))]
                [:rect
-                {:stroke :gainsboro
+                {:key nn
+                 :stroke :gainsboro
                  :stroke-width 0.5
                  :fill (if (contains?
                             #{0 2 4 5 7 9 11}
@@ -44,18 +102,20 @@
                  :y (nn->height nn)
                  :width width
                  :height height-per-stave}])
-             (for [tv (vals path-value-map)
-                   :let [pitch-set (pitch-set (tv/value tv))
-                         color (:color (tv/attrs tv))]
+             (for [v (values ast)
+                   :let [pitch-set (pitch-set (value v))
+                         active? (sub-path? active-path (:path v))
+                         color (or (:color (attrs v)) (when active? highlight-color))]
                    nn pitch-set
                    :when nn]
                [:rect
-                {:stroke :gainsboro
+                {:key (print-str (conj (:path v) nn))
+                 :stroke (if active? :black :gainsboro)
                  :stroke-width 0.5
                  :fill (or color :black)
-                 :x (rescale-tv-time (tv/start tv))
+                 :x (rescale-tv-time (start v))
                  :y (nn->height nn)
-                 :width (rescale-tv-time (tv/duration tv))
+                 :width (rescale-tv-time (duration v))
                  :height height-per-stave}]))]))
 
 (defn bracket [from to height]
@@ -86,77 +146,106 @@
 (defn translate [x y & children]
   (into [:g {:transform (str "translate(" x " " y ")")}] children))
 
-(defn voice-tree [{:keys [era width]}]
+(defn voice-tree [{:keys [era width height]}]
   (let [path-value-map (core/era->path-value-map era)
-        per-rung 10
-        rung-padding 5
-        rung-total (+ rung-padding per-rung) 
         metas (into {} (filter (fn [[path tv]] (keyword? (tv/value tv))) path-value-map)) ;; todo -> rewrite with specter
         max-count (apply max (map count (keys metas)))
+        per-rung (/ height (inc max-count))
+        rung-padding 0#_5
+        rung-total (+ rung-padding per-rung)
         last-end (last (sort (map (comp r/realize tv/end) (vals path-value-map))))
         rescale-tv-time (fn [tv-time] (float (utils.math/map-range (r/realize tv-time) 0 last-end 0 width)))
-        total-height (* rung-total (inc max-count))]
+        total-height height]
     [:svg
      {:width width
       :height total-height}
      (for [[path tv] metas]
-       (translate 0 (* rung-total (count path))
-                  [:text {:dominant-baseline :hanging
-                          :x (rescale-tv-time (tv/start tv))
-                          :font-family :helvetica
-                          :font-size 8
-                          :y 0}
-                   (str (name (tv/value tv)) (print-str path))]
-                  (bracket (rescale-tv-time (tv/start tv))
-                           (rescale-tv-time (tv/end tv))
-                           per-rung)))
+       [:g {:key (print-str path)}
+        (translate 0 (* rung-total (count path))
+                   [:text {:dominant-baseline :hanging
+                           :x (rescale-tv-time (tv/start tv))
+                           :font-family :helvetica
+                           :font-size 8
+                           :y 0}
+                    (str (name (tv/value tv)) (print-str path))]
+                   (bracket (rescale-tv-time (tv/start tv))
+                            (rescale-tv-time (tv/end tv))
+                            per-rung))])
      #_(translate 0 margin-height
-                (piano-roll width piano-roll-height path-value-map))]))
+                  (piano-roll width piano-roll-height path-value-map))]))
 
-(defn piano-roll [{:keys [era width height] :as attrs}] 
-  [piano-roll* width height (core/era->path-value-map era)])
+(defn piano-roll [{:keys [era width height] :as attrs}]
+  [piano-roll*
+   (merge attrs {:ast (leramir.ast/standard-interpretation era)})])
 
-(defn resizable-div* [_component _attrs]
-  (let [size (reagent.core/atom {:width 0 :height 300})
-        resize-observer (js/ResizeObserver. (fn [entries _] 
+(defn resizable-div* [initial-height _component _attrs]
+  (let [min-height 100
+        size (reagent.core/atom {:width 0 :height initial-height})
+        resize-observer (js/ResizeObserver. (fn [entries _]
                                               (when-let [entry (first entries)]
-                                                (.log js/console (.-target entry))
                                                 (let [dom-rect (.-contentRect entry)]
-                                                  (spy entry)
-                                                  (spy dom-rect)
+                                                  entry
+                                                  dom-rect
                                                   (reset! size {:width (.-width dom-rect)
-                                                                :height (spy (- (.-height dom-rect) 0 #_6.5))})))))]
-    (fn [component attrs]
+                                                                :height (.-height dom-rect)})))))]
+    (fn [_initial-height component attrs]
       (let [my-ref (react/useRef)]
         (react/useEffect
          (fn []
-           (println my-ref)
            (when (.-current my-ref)
              (.observe resize-observer (.-current my-ref)))
            (fn []
              (.disconnect resize-observer)))
          (clj->js []))
-        
+
         [:div.vertical-resize
          {:ref my-ref
-          :style {:height (:height @size)}}
+          :style {:height (:height @size)
+                  :min-height min-height}}
          [component (merge attrs @size)]]))))
 
-(defn resizable-div [component attrs]
-  [:f> resizable-div* component attrs])
+(defn resizable-div [initial-height component attrs]
+  [:f> resizable-div* initial-height component attrs])
 
 (defn visualize-era [{:keys [era width] :as attrs}]
   [:<>
-   [resizable-div voice-tree attrs]
-   [resizable-div piano-roll attrs]])
+   [resizable-div 100 voice-tree attrs]
+   [resizable-div 300 piano-roll attrs]])
 
 (defn editor [era on-change]
-  [visualize-era
-   {:era era
-    :on-change on-change}])
+  (let [active-path (reagent.core/atom [])]
+    (fn [era on-change]
+      (let [ast (leramir.ast/standard-interpretation era)
+            current-node (get-in ast (leramir.ast/ast-path @active-path))]
+        [:<>
+         [visualize-era
+          {:era era
+           :on-change on-change
+           :active-path @active-path}]
+         [:button
+          {:on-click #(when-let [d (down ast @active-path)]
+                        (spy d)
+                        (reset! active-path d))}
+          "down"]
+         [:button
+          {:on-click #(when-let [d (up ast @active-path)]
+                        (spy d)
+                        (reset! active-path d))}
+          "up"]
+         [:button
+          {:on-click #(when-let [d (left ast @active-path)]
+                        (spy d)
+                        (reset! active-path d))}
+          "left"]
+         [:button
+          {:on-click #(when-let [d (right ast @active-path)]
+                        (spy d)
+                        (reset! active-path d))}
+          "right"]
+         #_[pprint current-node]]))))
 
 (defn root []
-  (let [era (reagent.core/atom 
+  (let [era (reagent.core/atom
              [:era 2 3 #{4 5 6} [:heap 2
                                  [:era  2 3]
                                  [2 3]
@@ -166,7 +255,7 @@
        [:div.notebook
         [:h1 "UI ideas"]]
        [editor @era (fn [new-era]
-                      (reset! era new-era))] 
+                      (reset! era new-era))]
        [:br]
        [:svg]])))
 
@@ -175,18 +264,25 @@
   ;; todo -- navigation
   ;; cross siblings - 'horizontal'
   ;; accross levels - 'vertical'
+  ;; the basic ops are up, down, left right
 
   ;; todo -- zooming
   ;; you can zoom up to the current level that you're at
   ;; moving to a sibling shifts the zoom to the sibling
   ;; moving to a parent shifts the zoom to the parent
 
+  ;; value editing - read string a str, number, or set
+
+  ;; cycle era type
+
   ;; todo - editing
   ;; slup
   ;; barf
-  ;; moving pitch values up and down
-  ;; move up/down by octave
-  
+
+
+  ;; transpose by semitone
+  ;; variation - transpose by octave 
+
 
   (resizable-div
    (vizualize-era [[:graft {:color "green"} 2] 4 [5]]))
